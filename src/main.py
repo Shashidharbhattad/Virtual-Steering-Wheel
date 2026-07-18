b7bc67c0b581fd5b04d26b0856a00c8178774457
Shashidhar bhattad <shashidharbattad9535@gmail.com>
2026-07-18 22:27:10 +0530

    Step 3 - Implement real-time hand detection

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+import time
+
 
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
@@ -42,10 +44,39 @@ while True:
     if not ret:
         break
 
+    # Mirror image
+    frame = cv2.flip(frame, 1)
+
+    # Convert BGR → RGB
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Create MediaPipe Image
+    mp_image = mp.Image(
+        image_format=mp.ImageFormat.SRGB,
+        data=rgb_frame
+    )
+
+    # Timestamp in milliseconds
+    timestamp_ms = int(time.time() * 1000)
+
+    # Detect hands
+    result = detector.detect_for_video(mp_image, timestamp_ms)
+
+    # Number of hands
+    hand_count = len(result.hand_landmarks)
+
+    if hand_count > 0:
+        cv2.putText(
+            frame,
+            f"Hands: {hand_count}",
+            (20, 40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2
+        )
+
     cv2.imshow("Virtual Steering Wheel", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-
-cap.release()
-cv2.destroyAllWindows()
